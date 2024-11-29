fbb23b35ad617adbe99fc8039da4334d73c52fcf
jimasr <hazimasri2002@yahoo.com>
2024-11-29 09:17:35 +0100

    feat: ajout affichage de map

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
-import Map from "./components/home/map";
-import Navbar from "./components/home/navbar";
+import React from 'react';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 export default function Home() {
   return (
@@ -15,10 +17,19 @@ export default function Home() {
         priority
       />
       <main className={styles.main}>
-        <Map/>
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
+        <Map
+            style={{width: '800px', height: '500px'}}
+            defaultCenter={{lat: 46.54992, lng: 2.44}}
+            defaultZoom={3}
+            gestureHandling={'greedy'}
+            disableDefaultUI={true}
+            colorScheme='DARK'
+          />
+        </APIProvider>
       </main>
       <footer className={styles.footer}>
-          <p>© 2024 All rights reserved.</p>
+        <p>© 2024 All rights reserved.</p>
       </footer>
     </div>
   );
