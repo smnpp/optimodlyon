3c39a4f1bedb4ab2ec1b713d85720349fd9c65e7
jimasr <hazimasri2002@yahoo.com>
2024-12-05 22:26:35 +0100

    fix: modifie service loadMap et test

--- backend/optimodlyon/src/main/java/service/Service.java
+++ backend/optimodlyon/src/main/java/service/Service.java
@@ -5,6 +5,10 @@
  */
 package service;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import metier.Intersection;
 import metier.Map;
@@ -18,27 +22,33 @@ import util.FileType;
  */
 public class Service {
     
-    public Map loadMap(String fileName) {
+	public Map loadMap(String fileContent, String fileName) throws IOException {
 
-        // Déterminer le type de fichier
-        FileType fileType = FileParserFactory.determineFileType(fileName);
+		// Déterminer le type de fichier
+		File file = File.createTempFile("temp", ".xml");
+		file.deleteOnExit();
+		
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			writer.write(fileContent);
+		}
+		
+		FileType fileType = FileParserFactory.determineFileType(file);
+		// Vérifier si le type de fichier est bien XmlMap
+		if (fileType != FileType.XMLMAP) {
+			throw new IllegalArgumentException("Invalid file type for loading a map: " + fileType);
+		}
 
-        // Vérifier si le type de fichier est bien XmlMap
-        if (fileType != FileType.XMLMAP) {
-            throw new IllegalArgumentException("Invalid file type for loading a map: " + fileType);
-        }
+		// Récupérer le parser approprié via la factory
+		FileParser<HashMap<Long, Intersection>> parser = (FileParser<HashMap<Long, Intersection>>) FileParserFactory.getParser(fileType);
 
-        // Récupérer le parser approprié via la factory
-        FileParser<HashMap<Long, Intersection>> parser = (FileParser<HashMap<Long, Intersection>>) FileParserFactory.getParser(fileType);
+		// Parse le fichier et récupère la HashMap d'intersections
+		HashMap<Long, Intersection> intersectionMap = parser.parse(file);
 
-        // Parse le fichier et récupère la HashMap d'intersections
-        HashMap<Long, Intersection> intersectionMap = parser.parse(fileName);
+		// Construire l'objet Map à partir de la HashMap d'intersections
+		Map map = new Map(intersectionMap);
 
-        // Construire l'objet Map à partir de la HashMap d'intersections
-        Map map = new Map(intersectionMap);
-
-        return map;
-    }
+		return map;
+	}
 
 
 }
