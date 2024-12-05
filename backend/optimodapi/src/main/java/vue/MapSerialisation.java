7912084e071850e937dbc55a2091caf02a8f8af0
jimasr <hazimasri2002@yahoo.com>
2024-12-06 00:51:50 +0100

    feat: modifie service backend pour serialiser map

--- backend/optimodapi/src/main/java/vue/MapSerialisation.java
+++ backend/optimodapi/src/main/java/vue/MapSerialisation.java
@@ -8,8 +8,10 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import metier.Intersection;
 import metier.Map;
 
 /**
@@ -25,10 +27,23 @@ public class MapSerialisation extends Serialisation {
 
 		JsonObject container = new JsonObject();
 		Gson gson = new Gson();
-		String mapJson = gson.toJson(map);
 		
+		JsonObject simplifiedMap = new JsonObject();
+		HashMap<Long, Intersection> intersections = map.getIntersections();
+		for (Long id : intersections.keySet()) {
+			Intersection intersection = intersections.get(id);
+
+			JsonObject location = new JsonObject();
+			location.addProperty("latitude", intersection.getLocation().getLatitude());
+			location.addProperty("longitude", intersection.getLocation().getLongitude());
+
+			simplifiedMap.add(id.toString(), location);
+		}
+		
+		String simplifiedMapJson = gson.toJson(simplifiedMap);
+
 		container.addProperty("success", success);
-		container.addProperty("map", mapJson);
+		container.addProperty("map", simplifiedMapJson);
 
 		response.setContentType("application/json;charset=UTF-8");
 		PrintWriter out = response.getWriter();
