3ad5bf01c8eb57e45c841529d01604be3089adb2
jimasr <hazimasri2002@yahoo.com>
2024-12-07 17:57:12 +0100

    feat: changer serialisation et implementer affichage carte

--- frontend/app/services/service.tsx
+++ frontend/app/services/service.tsx
@@ -1,3 +1,5 @@
+import Intersection from '../types/intersection';
+
 class OptimodApiService {
 	private baseUrl: string;
 
@@ -5,7 +7,7 @@ class OptimodApiService {
 		this.baseUrl = process.env.NEXT_OPTIMODAPI_URL || 'http://localhost:10500/optimodapi';
 	}
 
-	async loadMap(file: File): Promise<any> {
+	async loadMap(file: File): Promise<Intersection[]> {
 		const fileName = file.name;
 		const fileContent = await this.readFileContent(file);
 
@@ -28,29 +30,32 @@ class OptimodApiService {
 			}
 			const data = await response.json();
 			console.log('data:', data);
-			// Process the data as needed
-			return data;
+			const map = data.map;
+
+			console.log('map:', map);
+
+			const intersections: Intersection[] = data.map.map((item: { id: string; location: { latitude: number; longitude: number } }) => {
+				const latitude = item.location.latitude;
+				const longitude = item.location.longitude;
+			
+				const location: google.maps.LatLngLiteral = {
+					lat: latitude,
+					lng: longitude
+				};
+			
+				return {
+					key: item.id,
+					location: location
+				};
+			});
+
+			return intersections;
 		} catch (error) {
 			console.error('Fetch error:', error);
 			throw error;
 		}
 	}
-
-	async testApi(): Promise<any> {
-		try {
-			const response = await fetch(`${this.baseUrl}${'/ActionServlet?action=test'}`);
-			if (!response.ok) {
-				throw new Error(`Error: ${response.statusText}`);
-			}
-			const data = await response.json();
-			console.log('data:', data);
-			return data;
-		} catch (error) {
-			console.error('Fetch error:', error);
-			throw error;
-		}
-	}
-
+	
 	private readFileContent(file: File): Promise<string> {
 		return new Promise((resolve, reject) => {
 			const reader = new FileReader();
